788eeb99d312b79ffe242726965874ef5ee06b61
Devu2004 <devansh7595@gmail.com>
2025-10-28 22:12:11 +0530

    fixing errors

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
-const server = require('./backend/src/app')
-require('dotenv').config()
-const connectToDB = require('./backend/src/db/db')
+const server = require('./src/app');
+require('dotenv').config();
+const connectToDB = require('./src/db/db');
 
-connectToDB()
-server.listen(process.env.PORT,()=>{
-    console.log('200 ok');
-})
+connectToDB();
+
+server.listen(process.env.PORT, () => {
+    console.log('Server running on port ' + process.env.PORT);
+});
